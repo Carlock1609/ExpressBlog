6c15519fed39527c3070b07f3f757bc3cdedb895
Jonathan Yates <carlock@Jonathans-MacBook-Pro.local>
2020-05-04 13:48:45 -0700

    changed folders

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 /**
  * Required External Modules
  */
-let express = require('express');
-let path = require('path');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
+// Mongoose example
+// const Schema = mongoose.Schema;
+// const BlogPost = new Schema({
+//     author: ObjectId,
+//     title: String,
+//     body: String,
+//     date: Date
+//   });
 /**
  * App Variables
  */
-let app = express();
-let port = process.env.PORT || '8000';
+// Let express know where to find templates
+// Lets express know what template engine we are using 'ejs'
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/ExpressBlog', { 
+													useNewUrlParser: true,
+													useUnifiedTopology: true,
+														});
+
+
+// let db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 /**
  *  App Configuration
  */
-app.get('/', (req,res) => {
-    res.status(200).send('YOU REACHED INDEX')
-});
 
+ 
+ 
 /**
  * Routes Definitions
  */
+app.get('/', (req,res) => {
+    res.render('home')
+});
+
+app.get('/user', (req,res) => {
+    res.render('userProfile')
+})
+
 
 /**
  * Server Activation
  */
+const port = process.env.PORT || "8000";
 app.listen(port, () => {
-    console.log('SERVER UP!!')
-})
+    console.log('SERVER IS UP!');
+});
