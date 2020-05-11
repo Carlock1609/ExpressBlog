4d9ca28345db2c3876697a054d290168257ea457
Jonathan Yates <carlock@Jonathans-MacBook-Pro.local>
2020-05-11 15:26:51 -0700

    stylesheets to css. Public js. Added link buttons

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -4,24 +4,9 @@ const User = require('../../models/user');
 
 router.get('/', function(req,res) {
     // Trying to figure out how to pass in map to display
-    async function getMap() {
-        let mymap = await L.map('mapid').setView([51.505, -0.09], 13);
-
-        L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${config.mapboxKey}`, {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-        }).addTo(mymap);
-    }
-    getMap()
-        .catch((err) => {
-            console.log('CAUGHT!!', err)
-        })
 
     context = {
-        'map': getMap(),
+
    }
     res.render('base', context)
 })
