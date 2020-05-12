1ba5977299ca87418d7d8d895e8785fb8c08435c
Jonathan Yates <carlock1609@gmail.com>
2020-05-11 21:15:47 -0700

    setting up onclick markers, figure out easier way

--- notes.md
+++ notes.md
@@ -14,3 +14,9 @@ db.users.find({username: 'name})[0].password -> users password
     - Dont display option to mark on map
 
 - Make navbar button animations on Pencode
+    - ***At a good point atm***
+
+- Figure out how to click on the map and initialize a marker with a text
+    - Maybe have the user click to get the lat/lng and then have them simply enter in text/file and submit
+        - Instead of trying to figure out how to do it alll at the same time.
+
--- src/public/js/map.js
+++ src/public/js/map.js
@@ -1,6 +1,5 @@
-
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
+// INITIALIZE map
+let mymap = L.map('mapid').setView([45.5051, -122.6750], 13);
 L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${config.mapboxKey}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -8,3 +7,23 @@ L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
 }).addTo(mymap);
+
+
+//  FIGURE OUT IF THE REST NEEDS TO BE IN THE ROUTE TO SAVE THE MARKERS
+// OTHERWISE HAVE USERS INPUT DATA, THEN USE ANOTHER FUNCTION TO DISPLAY THAT DATA,
+// INSTEAD OF USING THE ONCLICK AND SAVING DATA
+
+let marker = L.marker([45.5051, -122.6750]).addTo(mymap);
+marker.bindPopup("MURDER HORNET SIGHTING").openPopup();
+
+
+// THIS DISPLAYS AN ALERT lets try to figure out how to take the latlng and make a marker instead
+let popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Hornet Sight at " + e.latlng.toString())
+        .openOn(mymap);
+}
+mymap.on('click', onMapClick);
